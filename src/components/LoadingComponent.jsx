8e1f37d6c385816b7096453b7dbd43a2766d558e
jeffrey <jeffreyon11@gmail.com>
2023-01-20 00:07:47 +0100

    Installed react-spinners and setup a loading component

---
+++ src/components/LoadingComponent.jsx
@@ -0,0 +1,11 @@
+import BarLoader from 'react-spinners/BarLoader';
+
+function LoadingComponent() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <BarLoader />
+        </div>
+    );
+}
+
+export default LoadingComponent;
